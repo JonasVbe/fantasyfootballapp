d5238569a3ab6345fc94441b8b9558ae79aaf05e
Jonas Van Berlo <r0932274@student.thomasmore.be>
2024-01-04 13:46:10 +0100

    splash screen van voetbal

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,18 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Duur in milliseconden
+      launchAutoHide: true, // Automatisch verbergen na de duur
+      backgroundColor: 'rgba(128,208,10,0.95)', // Achtergrondkleur van de splash screen
+      androidSplashResourceName: 'splash', // Naam van de splash resource
+      showSpinner: true, // Toon een spinner (optioneel)
+      androidSpinnerStyle: 'large', // Stijl van de spinner op Android
+      iosSpinnerStyle: 'small', // Stijl van de spinner op iOS
+      spinnerColor: '#999999' // Kleur van de spinner
+    }
   }
 };
 
