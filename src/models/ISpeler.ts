1b8ddb2d2b63c628c8c2f56ac7e0f08fc127ea94
Jonas Van Berlo <r0932274@student.thomasmore.be>
2024-01-12 13:05:54 +0100

    spelerdata wordt opgehaald van api en weergegeven waar het weergegeven moet worden

--- src/models/ISpeler.ts
+++ src/models/ISpeler.ts
@@ -1,11 +1,15 @@
-export interface ISpeler {
+export interface ISpelerOLD {
   id: string
   naam: string
+  voornaam?: string | null
+  achternaam?: string | null
   ploeg: string
+  ploegId?: number | null
   positie: string
-  rugnummer: number
+  rugnummer?: number | null
   isActief: boolean
   isKapitein: boolean
   volgendeMatch: string
   logo?: string
+  rating?: number | null
 }
